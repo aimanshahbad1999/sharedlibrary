34bafd4c7cb37f472234534028f2edd1ffc08626
aimanshahbad1999 <aiman.shahbad@cuelogic.com>
2021-03-09 05:10:20 -0500

    updated 1

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,46 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
+def call(String name = 'Student') {
     echo "Hello, ${name}."
+	if(name=='Student'){
+		pipeline{
+			agent any
+			stage("learn"){	
+				steps{
+					echo "Learn any technology"
+				}
+			}
+			stage("Practice"){	
+				steps{
+					echo "Practice to explore more"
+				}
+			}
+			stage("Apply"){	
+				steps{
+					echo "Build Some Project "
+				}
+			}
+		}
+	}
+	else{
+		pipeline{
+			agent any
+			stage("1"){	
+				steps{
+					echo "Do whatever you want "
+				}
+			}
+			stage("2"){	
+				steps{
+					echo "Do whatever you want"
+				}
+			}
+			stage("3"){	
+				steps{
+					echo "Do whatever you want "
+				}
+			}
+		}
+		
+	}	
 }
